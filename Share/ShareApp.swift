5119bc7e55001e99064dee77b7abe6a212031980
David Lohle <Proplex@users.noreply.github.com>
2021-05-17 00:33:30 -0400

    Initial Commit

---
+++ Share/ShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareApp.swift
+//  Share
+//
+//  Created by David Lohle on 5/17/21.
+//
+
+import SwiftUI
+
+@main
+struct ShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
